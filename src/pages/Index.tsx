457bd99998765d92763b9b1c35f6830e61921d3f
Nathan Ruer <nath.ruer@gmail.com>
2025-09-30 21:23:03 +0200

    FIX: refresh router

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import Navigation from "@/components/ui/navigation";
+import React from 'react';
 import HeroSection from "@/components/pages/HomePage";
-import ProjectsSection from "@/components/pages/ProjectsPage";
-import SkillsSection from "@/components/pages/SkillsPage";
-import ContactSection from "@/components/pages/ContactPage";
 
 type Lang = 'fr' | 'en';
 
@@ -15,14 +11,7 @@ interface IndexProps {
 const Index: React.FC<IndexProps> = ({ currentLang, setCurrentLang }) => {
   return (
     <div className="min-h-screen">
-      <Navigation currentLang={currentLang} onLanguageChange={setCurrentLang} />
-
-      <Routes>
-        <Route path="/" element={<HeroSection currentLang={currentLang} />} />
-        <Route path="/projects" element={<ProjectsSection currentLang={currentLang} />} />
-        <Route path="/skills" element={<SkillsSection currentLang={currentLang} />} />
-        <Route path="/contact" element={<ContactSection currentLang={currentLang} />} />
-      </Routes>
+      <HeroSection currentLang={currentLang} />
     </div>
   );
 };
